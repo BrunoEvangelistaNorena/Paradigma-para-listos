4ed3a756da998187975a2836aa3c28bb8e275817
BrunoEvangelistaNorena <bruno.evangelista@upch.pe>
2025-09-17 08:44:51 -0500

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np  
 from datetime import time
-appointment=st.slider("programando la asesoria;",
-                       value=(time(11,30), time(12, 45)))
-st.write("esta agendado para:"  appointment)
+appointment=st.slider("Programando la asesoria:",
+                       value=(time(11,30), time(12, 45)),
+                      format="HH:mm")
+st.write("esta agendado para: {} a {}".format(appointment[0].strftime("%H:%M"), appointment[1].strftime("%H:%M")))
